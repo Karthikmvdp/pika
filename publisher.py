962c0ba03ea5c949344472e5fe0871805ddc4fbb
Karthikmvdp <37698063+Karthikmvdp@users.noreply.github.com>
2022-07-07 09:56:32 +0530

    Create publisher.py

---
+++ publisher.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import ssl
+import pika
+
+class BasicPikaClient:
+
+    def __init__(self, rabbitmq_broker_id, rabbitmq_user, rabbitmq_password, region):
+
+        # SSL Context for TLS configuration of Amazon MQ for RabbitMQ
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        ssl_context.set_ciphers('ECDHE+AESGCM:!ECDSA')
+
+        url = f"amqps://{rabbitmq_user}:{rabbitmq_password}@{rabbitmq_broker_id}.mq.{region}.amazonaws.com:5671"
+        parameters = pika.URLParameters(url)
+        parameters.ssl_options = pika.SSLOptions(context=ssl_context)
+
+        self.connection = pika.BlockingConnection(parameters)
+        self.channel = self.connection.channel()
+#!/usr/bin/python3
+#from basicClient import BasicPikaClient
+
+class BasicMessageSender(BasicPikaClient):
+
+    def declare_queue(self, queue_name):
+        print(f"Trying to declare queue({queue_name})...")
+        self.channel.queue_declare(queue=queue_name)
+
+    def send_message(self, exchange, routing_key, body):
+        channel = self.connection.channel()
+        channel.basic_publish(exchange=exchange,
+                              routing_key=routing_key,
+                              body=body)
+        print(f"Sent message. Exchange: {exchange}, Routing Key: {routing_key}, Body: {body}")
+
+    def close(self):
+        self.channel.close()
+        self.connection.close()
+
+if __name__ == "__main__":
+
+    # Initialize Basic Message Sender which creates a connection
+    # and channel for sending messages.
+    basic_message_sender = BasicMessageSender(
+        "b-8e6d8176-e3dc-4e5e-854c-dcb093946e3c",
+        "admin",
+        "anand12341234",
+        "us-east-1"
+    )
+
+    # Declare a queue
+    basic_message_sender.declare_queue("hello world queue")
+
+    # Send a message to the queue.
+    basic_message_sender.send_message(exchange="", routing_key="hello queue", body=b'Hello World')
+
+    # Close connections.
+    basic_message_sender.close()
